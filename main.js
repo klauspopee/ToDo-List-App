15cc3550f45357ee121820fc89b02cfa36f21240
unknown <saulgodman1999@gmail.com>
2026-06-07 15:35:49 +0100

    added stuff related to local Storage

--- main.js
+++ main.js
@@ -16,17 +16,71 @@ let tasksFinished = document.querySelector('.tasks-completed span');
 let deleteAllBtn = document.querySelector('.delete-all');
 let allFinishedBtn = document.querySelector('.all-finished'); 
 
+let arryOfTasks = [];
+
+
 //Focus on input feild
 window.onload = function () {
     Input.focus()
+    
 }
+
+if (localStorage.getItem('tasks')) {
+    arryOfTasks = JSON.parse(window.localStorage.getItem('tasks'));
+
+    // remove no tasks message
+    let noTasksMsg = document.querySelector('.no-tasks-message');
+    
+
+    // creat task elements from local storage 
+    if (arryOfTasks.length != 0) {
+
+        noTasksMsg.remove();
+
+
+        arryOfTasks.forEach(function(task) {
+            // creat span  element
+            let mainSpan = document.createElement('span');
+
+            //creat delete btn 
+            let deleteBtn = document.createElement('span');
+
+            // creat main span text
+            let text = document.createTextNode(task.content);
+
+            // creat the delete btn text
+            let deleteText = document.createTextNode('Delete');
+
+            // add text to span 
+            mainSpan.appendChild(text);
+
+            //add class to span
+            mainSpan.classList.add('task-box')
+
+            //add text to delete btn 
+            deleteBtn.appendChild(deleteText);
+
+            // add class to delete btn
+            deleteBtn.className = 'delete';
+
+            // add delete btn to main span 
+            mainSpan.appendChild(deleteBtn);
+            // add main span to task container
+            tasksContainer.appendChild(mainSpan); 
+            calculateTasks();
+        })
+    }
+    
+}
+
+
+
 
 // adding the task
 AddBtn.onclick = function () {
 
     //if input is empty
     if (Input.value === '') {
-        console.log('empty')
         // [1] add sweet alert here
         Swal.fire("Input Is Empty")
     } else {
@@ -52,8 +106,6 @@ AddBtn.onclick = function () {
         // creat main span text
         let text = document.createTextNode(Input.value);
 
-
-
         // creat the delete btn text
         let deleteText = document.createTextNode('Delete');
 
@@ -72,10 +124,25 @@ AddBtn.onclick = function () {
         // add delete btn to main span 
         mainSpan.appendChild(deleteBtn);
 
+
+
+        // creat the inique id 
+        let uniqueId = document.createAttribute('id');
+        uniqueId.value = Date.now()
+
+        // add attr to main span
+        mainSpan.setAttributeNode(uniqueId)
+
+
         // add task to the container 
         tasksContainer.appendChild(mainSpan); 
 
-        // window.localStorage.setItem('task',mainSpan.textContent);
+    
+
+        // add task to arry of tasks
+        addToArryTasks(Input.value,uniqueId.value);
+
+        
 
         // empty the input 
         Input.value = ''
@@ -95,6 +162,11 @@ document.addEventListener('click', function (e) {
 
         // remove current task
         e.target.parentNode.remove();
+        // remove task from arry
+        
+        deletTaskWithThisId(e.target.parentElement.getAttribute('id'))
+
+        localStorage.setItem('tasks', JSON.stringify(arryOfTasks))
 
         // check number of tasks inside the container
         if (tasksContainer.childElementCount == 0) {
@@ -102,10 +174,12 @@ document.addEventListener('click', function (e) {
         }
     }
 
+
     if (e.target.classList.contains('task-box')) {
         
         // toggle class 'finised'
-        e.target.classList.toggle('finished')
+        e.target.classList.toggle('finished');
+        
     }
 
     //calculate tasks
@@ -159,10 +233,35 @@ allFinishedBtn.onclick = function () {
     document.querySelectorAll('.task-box').forEach(function(task) {
         task.classList.add('finished')
     })
+    arryOfTasks.forEach(function(task) {
+        task.state = 'finished'
+    })
+    localStorage.setItem('tasks', JSON.stringify(arryOfTasks))
 }
 
 // delete all button 
 deleteAllBtn.onclick = function () {
     tasksContainer.replaceChildren();
+    localStorage.clear();
     creatNoTasks();
 }
+
+// add task to tasks arry and local storage function
+function addToArryTasks (taskText,taskId) {
+    let task = {
+        content: taskText,
+        id : taskId,
+        state: '',
+    }
+
+    
+    arryOfTasks.push(task)
+    localStorage.setItem('tasks', JSON.stringify(arryOfTasks))
+}
+
+// delete task from arry based on its id 
+function deletTaskWithThisId (taskId) {
+    arryOfTasks = arryOfTasks.filter((task) => task.id != taskId);
+    localStorage.setItem('tasks', JSON.stringify(arryOfTasks))
+}
+
